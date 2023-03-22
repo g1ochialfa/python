dae0f738ccd3c90e02de1ea0cba5c45abf81e02a
g1ochialfa <127632496+g1ochialfa@users.noreply.github.com>
2023-03-22 15:19:00 +0300

    Basic Rock,Paper,Scissors

---
+++ rock,papaer,scissors.py
@@ -0,0 +1,46 @@
+import random
+choices = ["rock","paper","scissors"]
+while True:
+    computer = random.choice(choices)
+    player = None
+    while player not in choices:
+        player = input("Rock,paper or scissors ?").lower()
+
+    if player == computer:
+
+        print("computer:",computer)
+        print("player:", player)
+        print("Tie")
+
+    elif player == "rock":
+        if computer =="paper":
+            print("computer",computer)
+            print("player:",player)
+            print("You lose!")
+        if computer == "scissors":
+            print("computer", computer)
+            print("player:", player)
+            print("You win!")
+    elif player == "scissors":
+        if computer == "paper":
+            print("computer", computer)
+            print("player:", player)
+            print("You win!")
+        if computer == "rock":
+            print("computer", computer)
+            print("player:", player)
+            print("You lose!")
+    elif player == "paper":
+        if computer == "rock":
+            print("computer", computer)
+            print("player:",player)
+            print("You win!")
+        if computer == "scissors":
+            print("computer", computer)
+            print("player:",player)
+            print("You lose!")
+    play_again = input("Play Again (yes/no)").lower()
+
+    if play_again == "no":
+        break
+print("BYE")
